d488309db0dd2756ad665e978c19848a2adeadd3
eoaooeoa <eoaooeoa@gmail.com>
2022-12-14 15:54:14 +0900

    feat. User 테이블 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entity/user.entity';
 import { UsersController } from './users/users.controller';
 
 @Module({
@@ -13,7 +14,7 @@ import { UsersController } from './users/users.controller';
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: 'test',
-      entities: [],
+      entities: [User],
       synchronize: true,
       autoLoadEntities: true,
       logging: true,
